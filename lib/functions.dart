69d4df7bc4263e6bef9638e964fc9af0d7b6ea4d
Kamil Paczkowski <37217644+ShamanDev@users.noreply.github.com>
2019-04-21 12:41:14 +0200

    Update functions.dart

--- lib/functions.dart
+++ lib/functions.dart
@@ -7,8 +7,11 @@ class MainScreen extends StatefulWidget{
 }
 
 class MainState extends State<MainScreen>{
-
   TextEditingController amountcontroller = TextEditingController();
+  static var _currencieswithshortcuts = {'Bulgarian Lev':'BGN', 'New Zeland Dollar':'NZD', 'Israeli New Shekel':'ILS', 'Russian Ruble':'RUB', 'Canadian Dollar':'CAD', 'American Dollar':'USD', 'Philippine Peso':'PHP', 'Swiss Franc': 'CHF', 'South African Rand':'ZAR', 'Australian Dollar':'AUD', 'Japanese Yen':'JPY', 'Turkish Lira':'TRY', 'Hong Kong Dollar':'HKD', 'Malaysian Ringgit':'MYR', 'Thai Baht':'THB', 'Croatian Kuna':'HRK', 'Norwegian Krone':'NOK', 'Indonesian Rupiah':'IDR', 'Danish Krone':'DKK', 'Czech Koruna':'CZK', 'Hungarian Forint':'HUF', 'British Pound Sterling':'GBP', 'Mexican Peso': 'MXN', 'South Korean Won':'KRW', 'Icelandic Króna': 'ISK', 'Singapore Dollar': 'SGD', 'Brazilian Real': 'BRL', 'Poland Złoty':'PLN', 'Indian Rupee':'INR', 'Romanian Leu':'RON', 'Chinese Yuan':'CNY', 'Swedish Krona':'SEK', 'Euro':'EUR'};
+  static var _currenciesnames = _currencieswithshortcuts.keys;
+  var _from = 'EUR';
+  var _to = 'USD';
   
   @override
   Widget build(BuildContext context) {
@@ -36,17 +39,24 @@ class MainState extends State<MainScreen>{
           //Background stuff end
 
           Center(
+            //The black rectangle start
+
             child: Container(
-              height: 350,
+              height: 330,
               width: 250,
               decoration: BoxDecoration(
                 color: Colors.black87,
-                borderRadius: BorderRadius.circular(6.0)
+                borderRadius: BorderRadius.circular(4.0)
               ),
-              child: ListView(
+
+            //Black rectangle end
+            
+              child: Stack(
                 children: <Widget>[
+
+                //Amount input start
                   Padding(
-                  padding: EdgeInsets.only(top: 25),
+                  padding: EdgeInsets.only(top: 25,left: 5, right: 5),
                   child:TextField(
                     keyboardType: TextInputType.number,
                     style: TextStyle(height: 0.3, fontSize: 18.0, color: Colors.white),
@@ -54,8 +64,8 @@ class MainState extends State<MainScreen>{
                       focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0), borderSide: BorderSide(color: Colors.red)),
                       enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0), borderSide: BorderSide(color: Colors.white)),
                       labelText: 'Amount',
-                      labelStyle: TextStyle(color: Colors.red),
-                      suffixText: "PLN",
+                      labelStyle: TextStyle(color: Colors.white),
+                      suffixText: _from,
                       suffixStyle: TextStyle(color: Colors.white)
                     ),
                     controller: amountcontroller,
@@ -63,6 +73,63 @@ class MainState extends State<MainScreen>{
                       debugPrint(amountcontroller.text);
                     },
                   )),
+                //Amount input finish
+              Padding(
+                padding: EdgeInsets.only(top:110, left: 99),
+                child:Text("From", style: TextStyle(color: Colors.white),),
+              ),
+
+              Padding(
+              padding: EdgeInsets.only(top: 120, left: 35),
+              child:DropdownButton<String>(
+                items:_currenciesnames.map((String value){
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+
+                  );
+                }).toList(),
+              
+                onChanged: (String value){
+                  setState(() {
+                    this._from = _currencieswithshortcuts[value];
+                    debugPrint(this._from);
+                  });
+                },
+
+                value: _currencieswithshortcuts[_from],
+
+              )),
+
+
+              Padding(
+                padding: EdgeInsets.only(top:180, left: 109),
+                child:Text("To", style: TextStyle(color: Colors.white),),
+              ),
+
+
+              Padding(
+              padding: EdgeInsets.only(top: 200, left: 35),
+              child:DropdownButton<String>(
+                items:_currenciesnames.map((String value){
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+
+                  );
+                }).toList(),
+              
+                onChanged: (String value){
+                  setState(() {
+                    this._to = _currencieswithshortcuts[value];
+                    debugPrint(this._to);
+                  });
+                },
+
+                value: _currencieswithshortcuts[_to],
+
+              )),
+  
                 ],
               )
             ),
